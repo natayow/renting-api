e76523ef20e49af4bd141e647b43a9e682d46457
natayow <nathasya1616@gmail.com>
2025-12-02 19:39:25 +0700

    api server & package library install

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
+import authrouter from './routers/auth.router';
+import { customCors } from './middlewares/cors.middleware';
 
 const app: Express = express();
+
+app.use(customCors);
+
 app.use(express.json());
 const port = 8000;
+
+app.use('/api/auth', authrouter);
 
 app.get('/', (_: Request, res: Response) => {
     res.send('<h1>Hello World!</h1>');
 });
 
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({
+    success: false, 
+    message: error?.message, 
+    data: null
+  })
+})
 
 
 app.listen(port, () => {
