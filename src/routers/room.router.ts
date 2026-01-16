c58b82039d9028afac7836fd9f973a98f1d3b462
natayow <nathasya1616@gmail.com>
2026-01-16 09:02:13 +0700

    feat(backend): peak season rate

--- src/routers/room.router.ts
+++ src/routers/room.router.ts
@@ -6,6 +6,14 @@ import {
     getRoomByIdController,
     updateRoomController,
     deleteRoomController,
+    getRoomAvailabilityController,
+    updateRoomAvailabilityController,
+    bulkUpdateRoomAvailabilityController,
+    getPeakSeasonRatesController,
+    createPeakSeasonRateController,
+    bulkCreatePeakSeasonRatesController,
+    updatePeakSeasonRateController,
+    deletePeakSeasonRateController,
 } from '../controllers/room.controller';
 import { jwtVerify, roleVerify } from '../middlewares/jwt-auth.middleware';
 import { JWT_SECRET_KEY } from '../config/main.config';
@@ -14,6 +22,16 @@ import {
     updateRoomValidation 
 } from '../validators/create-room.validator';
 import { handleValidationErrors } from '../middlewares/validator-request';
+import {
+    updateRoomAvailabilityValidation,
+    bulkUpdateRoomAvailabilityValidation,
+} from '../validators/room-availability.validator';
+import {
+    createPeakSeasonRateValidation,
+    bulkCreatePeakSeasonRatesValidation,
+    updatePeakSeasonRateValidation,
+    deletePeakSeasonRateValidation,
+} from '../validators/peak-season.validator';
 
 const router = Router();
 
@@ -44,6 +62,70 @@ router.delete(
     jwtVerify(JWT_SECRET_KEY),
     roleVerify(['ADMIN']),
     deleteRoomController
+);
+
+router.get(
+    '/:roomId/availability',
+    getRoomAvailabilityController
+);
+
+router.put(
+    '/:roomId/availability',
+    jwtVerify(JWT_SECRET_KEY),
+    roleVerify(['ADMIN']),
+    updateRoomAvailabilityValidation,
+    handleValidationErrors,
+    updateRoomAvailabilityController
+);
+
+router.post(
+    '/:roomId/availability/bulk',
+    jwtVerify(JWT_SECRET_KEY),
+    roleVerify(['ADMIN']),
+    bulkUpdateRoomAvailabilityValidation,
+    handleValidationErrors,
+    bulkUpdateRoomAvailabilityController
+);
+
+router.get(
+    '/:roomId/peak-season',
+    getPeakSeasonRatesController
+);
+
+router.post(
+    '/:roomId/peak-season',
+    jwtVerify(JWT_SECRET_KEY),
+    roleVerify(['ADMIN']),
+    createPeakSeasonRateValidation,
+    handleValidationErrors,
+    createPeakSeasonRateController
+);
+
+router.post(
+    '/:roomId/peak-season/bulk',
+    jwtVerify(JWT_SECRET_KEY),
+    roleVerify(['ADMIN']),
+    bulkCreatePeakSeasonRatesValidation,
+    handleValidationErrors,
+    bulkCreatePeakSeasonRatesController
+);
+
+router.put(
+    '/peak-season/:id',
+    jwtVerify(JWT_SECRET_KEY),
+    roleVerify(['ADMIN']),
+    updatePeakSeasonRateValidation,
+    handleValidationErrors,
+    updatePeakSeasonRateController
+);
+
+router.delete(
+    '/peak-season/:id',
+    jwtVerify(JWT_SECRET_KEY),
+    roleVerify(['ADMIN']),
+    deletePeakSeasonRateValidation,
+    handleValidationErrors,
+    deletePeakSeasonRateController
 );
 
 export default router;
